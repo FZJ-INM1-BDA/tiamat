94486d7c5f4ed4328f28e05232543c27fec05c88
fmatuschke <f.matuschke@fz-juelich.de>
2023-11-29 07:16:04 +0100

    RFC setup into pyproject.toml

--- setup.py
+++ setup.py
@@ -1,25 +1,6 @@
-# coding=utf-8
-from setuptools import setup, find_packages
-import versioneer
+""" Setup file for the package.
+"""
 
-setup(name="tiamat",
-      version=versioneer.get_version(),
-      packages=find_packages(),
-      author="Christian Schiffer",
-      author_email="c.schiffer@fz-juelich.de",
-      url="https://jugit.fz-juelich.de/inm-1/bda/tiamat/tiamat.git",
-      description="tiamat",
-      install_requires=[
-          "click",
-          "numpy>=1.25.0",
-          "opencv-python-headless>=4.8.1",
-          "pytiff",
-          "matplotlib",
-      ],
-      entry_points={
-          "console_scripts": [
-              "tiamat = tiamat.cli:cli",
-          ]
-      },
-      cmdclass=versioneer.get_cmdclass(),
-      )
+from setuptools import setup
+
+setup()
