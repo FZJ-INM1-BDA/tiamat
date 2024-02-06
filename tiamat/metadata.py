521b2fa0ee041cb55db392926d70b93ea057c101
Xiao Gui <xgui3783@gmail.com>
2024-02-06 13:48:23 +0100

    fix: add extents to image metadata

--- tiamat/metadata.py
+++ tiamat/metadata.py
@@ -3,6 +3,7 @@ Metadata on images.
 """
 from dataclasses import dataclass
 import numpy as np
+from itertools import repeat, product
 
 # Image types
 IMAGE_TYPE_SEGMENTATION = "segmentation"  # an image with discrete values, e.g., a mask or a segmentation.
@@ -27,3 +28,23 @@ class ImageMetadata:
     channel_dimension: int = 2  # Set to None for no channel dim
     channel_interpretation: str = CHANNEL_INTERPRETATION_COLOR
     additional_metadata: dict = None
+
+    @property
+    def extents(self):
+        """
+        Extents of the image. Provided as a list of tuple of coordinates. 
+        Extrapolated by shape. Can be set, and will then return the set value. 
+        Delete the property to revert to the default behavior.
+        """
+        if hasattr(self, '_extents'):
+            return self._extents
+        extents = list(zip(repeat(0), self.shape))
+        return list(product(*extents))
+    
+    @extents.setter
+    def extents(self, val):
+        self._extents = val
+
+    @extents.deleter
+    def extents(self):
+        delattr(self, '_extents')
