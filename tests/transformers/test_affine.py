705f2b4a33c062e44c825063abaf0d863dde4141
Xiao Gui <xgui3783@gmail.com>
2024-02-07 11:50:20 +0100

    test: add test to transform_access

--- tests/transformers/test_affine.py
+++ tests/transformers/test_affine.py
@@ -75,7 +75,11 @@ scale_double = [[2, 0, 0],
                 [0, 2, 0],
                 [0, 0, 1]]
 
-affine_args = [
+translate = [[1, 0, 5],
+             [0, 1, 10],
+             [0, 0, 1]]
+
+affine_xform_img_args = [
     (four_by_four, scale_half, two_by_two),
     (two_by_two, identity, two_by_two),
     (four_by_four, identity, four_by_four),
@@ -91,8 +95,9 @@ affine_args = [
       [4, 3]]),
 ]
 
-@pytest.mark.parametrize("src_img, affine, exp_img", affine_args)
-def test_affine_scale(src_img, affine, exp_img):
+
+@pytest.mark.parametrize("src_img, affine, exp_img", affine_xform_img_args)
+def test_affine_transform_image(src_img, affine, exp_img):
     img_nd = np.array(src_img)
     meta = ImageMetadata(image_type="image",
                          shape=img_nd.shape,
@@ -102,3 +107,44 @@ def test_affine_scale(src_img, affine, exp_img):
     xform = AffineTransformer(np.array(affine))
     result = xform.transform_image(src)
     assert np.all(result.image == np.array(exp_img))
+
+IMG_SIZE = 100
+
+mirror = [[-1, 0, IMG_SIZE - 1],
+          [0, 1, 0],
+          [0, 0, 1]]
+
+affine_xform_xform_xs_args = [
+    (((0, 2), (0, 2)), identity, ((0, 2), (0, 2))),
+    (((5, 10), (15, 20)), identity, ((5, 10), (15, 20))),
+
+    (((0, 2), (0, 2)), scale_double, ((0, 1), (0, 1))),
+    (((5, 10), (15, 20)), scale_double, ((3, 5), (8, 10))), # np.ceil
+
+    
+    (((0, 2), (0, 2)), scale_half, ((0, 4), (0, 4))),
+    (((5, 10), (15, 20)), scale_half, ((10, 20), (30, 40))),
+
+    
+    (((0, 2), (0, 2)), translate, ((-5, -3), (-10, -8))),
+    (((5, 10), (15, 20)), translate, ((0, 5), (5, 10))),
+    
+    (((0, 2), (0, 2)), mirror, ((97, 99), (0, 2))),
+    (((5, 10), (15, 20)), mirror, ((89, 94), (15, 20))),
+]
+
+
+@pytest.mark.parametrize("src_xy, affine, expected_xy", affine_xform_xform_xs_args)
+def test_affine_transform_access(src_xy, affine, expected_xy):
+    img_nd = np.zeros((IMG_SIZE, IMG_SIZE))
+    meta = ImageMetadata(image_type="image",
+                         shape=img_nd.shape,
+                         value_range=(np.min(img_nd), np.max(img_nd)),
+                         dtype=img_nd.dtype)
+    accessor = ImageAccessor(*src_xy, metadata=meta)
+    xformer = AffineTransformer(np.array(affine))
+    result = xformer.transform_access(accessor)
+
+    ex_x, ex_y, *_ = expected_xy
+    assert result.x == ex_x
+    assert result.y == ex_y
