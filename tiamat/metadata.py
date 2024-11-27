e97bab8afa221458eeddae00a82a14a1d410ca68
a.oberstrass <a.oberstrass@fz-juelich.de>
2024-11-27 17:46:19 +0100

    Allow variable image dimensions and channels in image access

--- tiamat/metadata.py
+++ tiamat/metadata.py
@@ -24,6 +24,6 @@ class ImageMetadata:
     dtype: np.dtype
     file_path: str = None
     spacing: float | tuple[float, ...] = None
-    channel_dimension: int = 2
+    channel_dimension: int = 2  # Set to None for no channel dim
     channel_interpretation: str = CHANNEL_INTERPRETATION_COLOR
     additional_metadata: dict = None
