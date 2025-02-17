124505de990ed3d4fdfd5a5385b23edd154b0ef0
a.oberstrass <a.oberstrass@fz-juelich.de>
2025-02-17 14:56:56 +0100

    Implement antialiasing for rescale

--- tiamat/io.py
+++ tiamat/io.py
@@ -27,6 +27,10 @@ class ImageAccessor:
     coordinate_spacing: float = 1.0
     metadata: ImageMetadata = None
     interpolation: int = None
+    # Wether to apply Gauss smothing, default is False
+    anti_aliasing: bool = False
+    # (Maybe not needed) Std of Gauss filter, default is (s - 1) / 2:
+    # anti_aliasing_sigma: float = None
     history: dict = field(default_factory=dict)
 
 
