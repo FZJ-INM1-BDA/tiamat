5ad152a8b08e61c203776ce02f80b965a36ed7f6
Christian Schiffer <c.schiffer@fz-juelich.de>
2025-03-20 16:22:21 +0100

    implemented size

--- examples/generic_array.py
+++ examples/generic_array.py
@@ -6,7 +6,7 @@ from tiamat.pipeline import Pipeline
 from tiamat.array import Array
 
 array = Array(file_name="./data/Koala.jpg", pipeline=Pipeline(), scale=1.0)
-print(array.shape, array.dtype)
+print(array.shape, array.dtype, array.ndim, array.size)
 print("row", array[42].shape)
 print("col", array[:, 42].shape)
 print("channel", array[..., 0].shape)
