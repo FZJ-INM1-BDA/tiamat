4ddce03bab276c253506912d5cda18e9456a69cb
a.oberstrass <a.oberstrass@fz-juelich.de>
2025-03-20 22:27:24 +0100

    Share _resolve_coordinate function in processing.py

--- tiamat/readers/processing.py
+++ tiamat/readers/processing.py
@@ -145,6 +145,17 @@ def _prepare_coordinates(
         prepared.append(prepared_coord)
     return prepared
 
+def _resolve_coordinate(coordinate, image_dimension):
+
+    if coordinate is None:
+        return image_dimension
+    elif isinstance(coordinate, (np.integer, int)):
+        return coordinate
+    elif isinstance(coordinate, (np.floating, float)):
+        raise RuntimeError(f"Encountered fractional value of {coordinate} as coordinate, which is not supported.")
+    else:
+        return tuple(_resolve_coordinate(coordinate_i, image_dimension) for coordinate_i in coordinate)
+
 
 def _zero_clip(values):
     return [max(value, 0) if value is not None else None for value in values]
