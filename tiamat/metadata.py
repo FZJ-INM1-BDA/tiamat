debd855c856f48fc6df2460a0f6baea4cd196084
Christian Schiffer <c.schiffer@fz-juelich.de>
2025-03-20 15:26:36 +0100

    Added scales to the metadata

--- tiamat/metadata.py
+++ tiamat/metadata.py
@@ -1,13 +1,19 @@
 """
 Metadata on images.
 """
+
 from dataclasses import dataclass
+from typing import Iterable
 import numpy as np
 from itertools import repeat, product
 
 # Image types
-IMAGE_TYPE_SEGMENTATION = "segmentation"  # an image with discrete values, e.g., a mask or a segmentation.
-IMAGE_TYPE_IMAGE = "image"  # an image with continuous values (e.g., a microscopy image).
+IMAGE_TYPE_SEGMENTATION = (
+    "segmentation"  # an image with discrete values, e.g., a mask or a segmentation.
+)
+IMAGE_TYPE_IMAGE = (
+    "image"  # an image with continuous values (e.g., a microscopy image).
+)
 
 # Meaning of channel dimension
 CHANNEL_INTERPRETATION_COLOR = "color"
@@ -19,6 +25,7 @@ class ImageMetadata:
     """
     Metadata of an image.
     """
+
     image_type: str
     shape: tuple
     value_range: tuple
@@ -28,6 +35,7 @@ class ImageMetadata:
     channel_dimension: int = None  # None means no channel dimension
     channel_interpretation: str = CHANNEL_INTERPRETATION_COLOR
     additional_metadata: dict = None
+    scales: float | int | Iterable[float | int] | None = None
 
     @property
     def spatial_dimensions(self):
@@ -40,19 +48,19 @@ class ImageMetadata:
     @property
     def extents(self):
         """
-        Extents of the image. Provided as a list of tuple of coordinates. 
-        Extrapolated by shape. Can be set, and will then return the set value. 
+        Extents of the image. Provided as a list of tuple of coordinates.
+        Extrapolated by shape. Can be set, and will then return the set value.
         Delete the property to revert to the default behavior.
         """
-        if hasattr(self, '_extents'):
+        if hasattr(self, "_extents"):
             return self._extents
         extents = list(zip(repeat(0), self.shape))
         return list(product(*extents))
-    
+
     @extents.setter
     def extents(self, val):
         self._extents = val
 
     @extents.deleter
     def extents(self):
-        delattr(self, '_extents')
+        delattr(self, "_extents")
