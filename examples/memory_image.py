6019cac1e93eab919c9c7e4a9f820183671525f3
a.oberstrass <a.oberstrass@fz-juelich.de>
2025-08-07 22:30:50 +0200

    Remove manual set metadata in examples

--- examples/memory_image.py
+++ examples/memory_image.py
@@ -16,31 +16,31 @@ metadata = reader.read_metadata()
 print(metadata)
 
 # read the entire image
-crop = reader.read_image(ImageAccessor(scale=1.0, metadata=metadata))
+crop = reader.read_image(ImageAccessor(scale=1.0))
 print(crop.image.shape)
 
-crop = reader.read_image(ImageAccessor(x=x, y=y, scale=1.0, metadata=metadata))
+crop = reader.read_image(ImageAccessor(x=x, y=y, scale=1.0))
 print(crop.image.shape)
 
-crop = reader.read_image(ImageAccessor(x=x, y=x, c=1, scale=1.0, metadata=metadata))
+crop = reader.read_image(ImageAccessor(x=x, y=x, c=1, scale=1.0))
 print(crop.image.shape)
 
 crop = reader.read_image(
-    ImageAccessor(x=(0, 100), y=y, c=1, scale=1.0, metadata=metadata)
+    ImageAccessor(x=(0, 100), y=y, c=1, scale=1.0)
 )
 print(crop.image.shape)
 
 crop = reader.read_image(
-    ImageAccessor(x=(0, 100), y=(0, 100), c=1, scale=1.0, metadata=metadata)
+    ImageAccessor(x=(0, 100), y=(0, 100), c=1, scale=1.0)
 )
 print(crop.image.shape)
 
 crop = reader.read_image(
-    ImageAccessor(x=(0, 100), y=(0, 100), c=(0, 2), scale=1.0, metadata=metadata)
+    ImageAccessor(x=(0, 100), y=(0, 100), c=(0, 2), scale=1.0)
 )
 print(crop.image.shape)
 
 crop = reader.read_image(
-    ImageAccessor(x=(0, 100), y=(0, 100), scale=0.5, metadata=metadata)
+    ImageAccessor(x=(0, 100), y=(0, 100), scale=0.5)
 )
 print(crop.image.shape)
