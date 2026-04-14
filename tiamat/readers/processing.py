ef26082dbaaf332a2bd2240da83a9911fcd65187
a.oberstrass <a.oberstrass@fz-juelich.de>
2026-04-14 17:32:27 +0200

    Fix expand length 1d numpy arrays

--- tiamat/readers/processing.py
+++ tiamat/readers/processing.py
@@ -49,9 +49,16 @@ def expand_to_length(value: Any, length: int) -> list[Any]:
     Returns:
         List of length `length`.
     """
-    if not isinstance(value, (list, tuple, np.ndarray)):
-        return [value] * length
-    elif len(value) == 1:
+
+    print(value)
+    print(type(value))
+
+    if np.ndim(value) == 0:
+        if isinstance(value, np.ndarray):
+            return [value.item()] * length
+        else:
+            return [value] * length
+    if len(value) == 1:
         return [value[0]] * length
     return value
 
